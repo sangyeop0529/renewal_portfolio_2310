f482e3ca294312c32ca72a59949a2168028c7bf9
DELL <sangyeop0529@gmail.com~>
2023-10-31 17:25:41 +0900

    jumpit 적용

--- js/typeit.js
+++ js/typeit.js
@@ -2,19 +2,24 @@
 
 new TypeIt(".home__title--strong", {
   loop: true,
-  speed: 50,
-}) // 노력하는 웹퍼블리셔|
-  .move(-11)
-  .type("항샹 ")
+  speed: 200,
+}) // 꿈꾸는 웹퍼블리셔|
+  .move(-6)
+  .delete(5)
+  .type("도전하는")
   .pause(1000)
   .move(null, { to: "END" })
   .delete()
-  .type("프론트엔드 엔지니어")
-  .move(-5)
-  .delete(5)
-  .type("백엔드")
-  .delete(5)
-  .type("풀스택")
-  .move(11)
-  .delete()
+  // .type("항샹 ")
+  // .pause(1000)
+  // .move(null, { to: "END" })
+  // .delete()
+  // .type("프론트엔드 엔지니어")
+  // .move(-5)
+  // .delete(5)
+  // .type("백엔드")
+  // .delete(5)
+  // .type("풀스택")
+  // .move(11)
+  // .delete()
   .go();
